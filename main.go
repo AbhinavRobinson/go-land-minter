f122228396f9f10e300bae601ec7a602f94b081f
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-17 16:40:11 +0530

    🧩 feat(init): add env and middlewares

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/joho/godotenv"
+
+	"os"
 )
 
 func main() {
+	// Load ENV
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+	// Load Fiber
 	app := fiber.New()
 	// Middlewares
 	app.Use(cors.New())
@@ -16,7 +28,10 @@ func main() {
 	// Routes
 	setupRoutes(app)
 	// Listen
-	err := app.Listen("127.0.0.1:3000")
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	err = app.Listen(fmt.Sprintf("%s:%s", host, port))
+	// Catch error (should not happen)
 	if err != nil {
 		return
 	}
