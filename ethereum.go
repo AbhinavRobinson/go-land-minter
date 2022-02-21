0370bf9738581229527ec950cab0d8286082db57
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-21 17:38:30 +0530

    🧩 feat(api): init contracts

--- ethereum.go
+++ ethereum.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	LAND "go_minter/contracts/land"
+	SALE "go_minter/contracts/sale"
 	TOKEN "go_minter/contracts/token"
 	"log"
 	"os"
@@ -13,6 +15,9 @@ import (
 )
 
 var token *TOKEN.Token
+var land *LAND.Main
+var sale *SALE.Main
+
 var cl *ethclient.Client
 
 var ctx = context.Background()
@@ -20,7 +25,11 @@ var callOpts = &bind.CallOpts{Context: ctx, Pending: false}
 
 func setupContracts() {
 	provider := os.Getenv("PROVIDER")
-	address := common.HexToAddress("4E0732efCdF0Cf92C48439535CD763de06FE353a")
+	tokenAddress := common.HexToAddress("4E0732efCdF0Cf92C48439535CD763de06FE353a")
+	landAddress := common.HexToAddress("5f1994949073dA9252D79ee53e3C91255Dc90070")
+	saleAddress := common.HexToAddress("751d2375C2Db72Af22350Db58c3F44a89F03e08c")
+
+	// INIT ETH CLIENT
 
 	client, errClient := ethclient.Dial(provider)
 	if errClient != nil {
@@ -28,16 +37,47 @@ func setupContracts() {
 	}
 	cl = client
 
-	t, errToken := TOKEN.NewToken(address, client)
+	// LOAD CONTRACTS
+
+	t, errToken := TOKEN.NewToken(tokenAddress, client)
 	if errToken != nil {
-		log.Fatalf("Some error occurred. Err: %s", errToken)
+		log.Fatalf("Some error occurred in TOKEN. Err: %s", errToken)
 	}
 	token = t
+
+	l, errLand := LAND.NewMain(landAddress, client)
+	if errLand != nil {
+		log.Fatalf("Some error occurred in LAND. Err: %s", errLand)
+	}
+	land = l
+
+	s, errSale := SALE.NewMain(saleAddress, client)
+	if errSale != nil {
+		log.Fatalf("Some error occurred in SALE. Err: %s", errSale)
+	}
+	sale = s
+
+	// CHECK IF LOADED
 
 	tokenName, err := token.Name(callOpts)
 	if err != nil {
 		log.Fatalf("Some error occurred. Err: %s", err)
 	}
 	fmt.Printf("Token Loaded: %s\n", tokenName)
+
+	landName, err := land.Name(callOpts)
+	if err != nil {
+		log.Fatalf("Some error occurred. Err: %s", err)
+	}
+	fmt.Printf("Land Loaded: %s\n", landName)
+
+	saleTokenName, err := sale.TOKEN(callOpts)
+	if err != nil {
+		log.Fatalf("Some error occurred. Err: %s", err)
+	}
+	fmt.Printf("Sale Loaded with Token: %s\n", saleTokenName)
+
+	// SUCCESS
+
 	fmt.Println("🚀 Contracts are loaded!")
 }
