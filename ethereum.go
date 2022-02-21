ea6eb78588236813d0ae4921827037fa5a9243b7
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-21 16:16:27 +0530

    🧱 feat(api): add contracts

--- ethereum.go
+++ ethereum.go
@@ -3,15 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	TOKEN "go_minter/contracts/token"
 	"log"
 	"os"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-var token *Token
+var token *TOKEN.Token
 var cl *ethclient.Client
 
 var ctx = context.Background()
@@ -19,20 +20,23 @@ var callOpts = &bind.CallOpts{Context: ctx, Pending: false}
 
 func setupContracts() {
 	provider := os.Getenv("PROVIDER")
+	address := common.HexToAddress("4E0732efCdF0Cf92C48439535CD763de06FE353a")
+
 	client, errClient := ethclient.Dial(provider)
 	if errClient != nil {
-		log.Fatal(errClient)
+		log.Fatalf("Error connecting to client: %s", errClient)
 	}
 	cl = client
-	address := common.HexToAddress("4E0732efCdF0Cf92C48439535CD763de06FE353a")
-	t, errToken := NewToken(address, client)
+
+	t, errToken := TOKEN.NewToken(address, client)
 	if errToken != nil {
-		log.Fatal(errToken)
+		log.Fatalf("Some error occurred. Err: %s", errToken)
 	}
 	token = t
+
 	tokenName, err := token.Name(callOpts)
 	if err != nil {
-		log.Fatalf("Some error occured. Err: %s", err)
+		log.Fatalf("Some error occurred. Err: %s", err)
 	}
 	fmt.Printf("Token Loaded: %s\n", tokenName)
 	fmt.Println("🚀 Contracts are loaded!")
